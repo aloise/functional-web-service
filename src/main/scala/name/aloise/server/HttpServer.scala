33efb291c27d17f164793b00c90952f738744d99
Ihor <erwaht@gmail.com>
2019-02-09 22:59:34 +0100

    formatting

--- src/main/scala/name/aloise/server/HttpServer.scala
+++ src/main/scala/name/aloise/server/HttpServer.scala
@@ -1,14 +1,12 @@
 package name.aloise.server
 
 import cats.effect._
-import cats.implicits._
-import org.http4s.{HttpApp, HttpRoutes}
-import org.http4s.blaze.http.HttpService
-import org.http4s.server.Server
+import org.http4s.HttpRoutes
 import org.http4s.implicits._
+import org.http4s.server.Server
 import org.http4s.server.blaze.BlazeServerBuilder
 
-case class HttpServer[F[_]: Effect : ConcurrentEffect : Timer](services: HttpRoutes[F])(host: String = "localhost", port:Int = 8080) {
+case class HttpServer[F[_] : ConcurrentEffect : Timer](services: HttpRoutes[F])(host: String = "localhost", port: Int = 8080) {
 
   val server: Resource[F, Server[F]] = BlazeServerBuilder[F]
     .bindHttp(port, host)
