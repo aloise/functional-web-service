f59e61b4bb576e95ba804ed5bd4c588ab6928109
Ihor <erwaht@gmail.com>
2019-02-09 22:29:27 +0100

    health service

--- src/main/scala/name/aloise/service/HealthService.scala
+++ src/main/scala/name/aloise/service/HealthService.scala
@@ -3,20 +3,22 @@ package name.aloise.service
 import cats.effect.Sync
 import io.circe.ObjectEncoder
 import org.http4s.HttpRoutes
-import org.http4s._
 import io.circe.syntax._
-import org.http4s.dsl.io._
 import name.aloise.build.BuildInfo
 import name.aloise.service.HealthService.Health
 import io.circe.generic.semiauto.deriveEncoder
+import org.http4s.dsl.Http4sDsl
 
-case class HealthService[F[_] : Sync]() {
+
+case class HealthService[F[_] : Sync]() extends Http4sDsl[F]{
+
+  import org.http4s.circe._
 
   private val healthResponse = Health(BuildInfo.name, BuildInfo.version).asJson
 
   val routes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root =>
-      Ok("123") //
+      Ok(healthResponse)
   }
 }
 
