d5d60e631629b971c7fb51a4691a3e6092611c1c
Ihor <erwaht@gmail.com>
2019-03-31 21:49:17 +0200

    dependencies - with merger

--- modules/common/src/main/scala/name/aloise/core/Dep.scala
+++ modules/common/src/main/scala/name/aloise/core/Dep.scala
@@ -1,14 +1,10 @@
 package name.aloise.core
 
-import shapeless.union.Union
-import shapeless.{HList, HNil}
-
 trait Dep[A] { self =>
 
-  type Requires <: HList
+  type Requires
 
   def provide(d: Requires): A
-
 
   def map[B](f: A => B): Dep[B] = new Dep[B]{ x =>
     override type Requires = self.Requires
@@ -16,10 +12,33 @@ trait Dep[A] { self =>
     override def provide(d: Requires): B = f(self.provide(d))
   }
 
-  def flatMap[B, Req2, BothReq](f: A => Dep.Aux[Req2, ])
+  // todo - hlist union
+
+  def flatMap[B, Req2, BothReq](f: A => Dep.Aux[Req2, B])
+                               (implicit merger: Merger.Aux[self.Requires, Req2, BothReq]): Dep.Aux[BothReq, B] = new Dep[B]{
 
     override type Requires = BothReq
+
+    override def provide(d: BothReq): B = {
+      val (d0, d1) = merger.split
+      f(self.provide(d0)).provide(d1)
+    }
   }
+}
+
+
+trait Merger[D0, D1] {
+
+  type Out
+
+  def merge(dep1: D0, dep2: D1): Merger[D0, D1]
+
+  def split: (D0, D1)
+
+}
+
+object Merger {
+  type Aux[D0, D1, Out2] = Merger[D0, D1]{ type Out = Out2}
 }
 
 
