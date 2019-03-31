a680684bba6b46d0cb5783d061cf6982ef20b3c8
Ihor <erwaht@gmail.com>
2019-03-31 22:14:28 +0200

    dependencies - with merger for hlist - part 2

--- modules/common/src/main/scala/name/aloise/core/Dep.scala
+++ modules/common/src/main/scala/name/aloise/core/Dep.scala
@@ -1,7 +1,5 @@
 package name.aloise.core
 
-import shapeless.ops.hlist.Prepend
-
 trait Dep[A] { self =>
 
   type Requires
@@ -14,44 +12,49 @@ trait Dep[A] { self =>
     override def provide(d: Requires): B = f(self.provide(d))
   }
 
-  // todo - hlist union
-
   def flatMap[B, Req2, BothReq](f: A => Dep.Aux[Req2, B])
                                (implicit merger: Merger.Aux[self.Requires, Req2, BothReq]): Dep.Aux[BothReq, B] = new Dep[B]{
 
     override type Requires = BothReq
 
     override def provide(d: BothReq): B = {
-      val (d0, d1) = merger.split
+      val (d0, d1) = merger.split(d)
       f(self.provide(d0)).provide(d1)
     }
   }
 }
 
 
-trait Merger[D0, D1] {
+abstract class Merger[D0, D1] {
 
   type Out
 
   def merge(dep1: D0, dep2: D1): Out
 
-  def split: (D0, D1)
+  def split(merged: Out): (D0, D1)
 
 }
 
 object Merger {
 
-
   type Aux[D0, D1, Out2] = Merger[D0, D1]{ type Out = Out2}
 
   import shapeless.HList
+  import shapeless.ops.hlist.Prepend
+  import shapeless.ops.hlist.Split
+  import shapeless.ops.hlist.Length
+  import shapeless.Nat
 
-  implicit def implicitHListMerger[D0 <: HList, D1 <: HList, Result <: HList](implicit prepend: Prepend.Aux[D0, D1, Result]): Merger.Aux[D0, D1, Result] = new Merger[D0, D1] {
+  implicit def implicitHListMerger[D0 <: HList, D1 <: HList, Result <: HList, N <: Nat](
+      implicit prepend: Prepend.Aux[D0, D1, Result],
+      len: Length[D0],
+      split: Split.Aux[Result, N, D0, D1]
+  ): Merger.Aux[D0, D1, Result] = new Merger[D0, D1] {
     override type Out = Result
 
     override def merge(dep1: D0, dep2: D1): Out = prepend(dep1, dep2)
 
-    override def split: (D0, D1) = ???
+    override def split(merged: Out): (D0, D1) = split(merged)
   }
 
 }
