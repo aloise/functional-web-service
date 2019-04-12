703fb846cd4d2a3f469a0de3d61b4e27e82215b0
Ihor <erwaht@gmail.com>
2019-04-12 18:42:11 +0200

    refactoring - helper methods

--- modules/common/src/main/scala/name/aloise/core/Dep.scala
+++ modules/common/src/main/scala/name/aloise/core/Dep.scala
@@ -4,10 +4,9 @@ import cats.{FlatMap, Functor, Id}
 import name.aloise.core.utils.hlist.Detach
 import shapeless._
 import shapeless.ops.hlist.Union
-
 import scala.language.higherKinds
 
-trait DepT[F[_], A] { self ⇒
+trait DepT[F[_], A] { self =>
 
   type Env <: HList
 
@@ -22,12 +21,12 @@ trait DepT[F[_], A] { self ⇒
   def run(implicit ev: HNil =:= Env): F[A] =
     runWithEnv(ev(HNil))
 
-  def map[B](f: A ⇒ B)(implicit functorF: Functor[F], dist: IsDistinctConstraint[Env]): DepT.Aux[Env, F, B] = new DepT[F, B] {
+  def map[B](f: A => B)(implicit functorF: Functor[F], dist: IsDistinctConstraint[Env]): DepT.Aux[Env, F, B] = new DepT[F, B] {
     override type Env = self.Env
     override def runWithEnv(environment: Env): F[B] = functorF.map(self.runWithEnv(environment))(f)
   }
 
-  def flatMap[B, Env2 <: HList, CombinedEnv <: HList](f: A ⇒ DepT.Aux[Env2, F, B])(
+  def flatMap[B, Env2 <: HList, CombinedEnv <: HList](f: A => DepT.Aux[Env2, F, B])(
     implicit unionOp: Union.Aux[Env, Env2, CombinedEnv],
     detach: Detach[Env, Env2, CombinedEnv],
     flatMapF: FlatMap[F]): DepT.Aux[CombinedEnv, F, B] = new DepT[F, B] {
@@ -36,15 +35,23 @@ trait DepT[F[_], A] { self ⇒
 
     override def runWithEnv(environment: CombinedEnv): F[B] = {
       val (env1, env2) = detach(environment)
-      flatMapF.flatMap(self.runWithEnv(env1))(a ⇒ f(a).runWithEnv(env2))
+      flatMapF.flatMap(self.runWithEnv(env1))(a => f(a).runWithEnv(env2))
     }
   }
-
-
 }
 
 object DepT {
-  type Aux[Env, F[_], A] = DepT[F, A] { type Requires = Env }
+  type Aux[E, F[_], A] = DepT[F, A] { type Env = E }
+
+  def pure[F[_], A](value: => F[A]): DepT.Aux[HNil, F, A] = new DepT[F, A] {
+    override type Env = HNil
+    override def runWithEnv(env: Env): F[A] = value
+  }
+
+  def inject[F[_]: FlatMap, E, A](reader: E => F[A]): DepT.Aux[E :: HNil, F, A] = new DepT[F, A] {
+    override type Env = E :: HNil
+    override def runWithEnv(env: Env): F[A] = reader(env.head)
+  }
 }
 
 trait Dep[A] extends DepT[Id, A]
@@ -53,7 +60,7 @@ object Dep {
 
   type Aux[Req0, A0] = DepT.Aux[Req0, cats.Id, A0]
 
-
-
-
+  def pure[A](value: => A) = DepT.pure[Id, A](value)
+  def inject[E, A](reader: E => A) = DepT.inject[Id, E, A](reader)
+  
 }
